29f83451d1cb5902cea9642f782ced240c94c2bb
leenayeonnn <skdus4190@gmail.com>
2024-03-25 15:27:10 +0900

    Feat: 생성자 추가

--- src/com/example/day05/util/Dice.java
+++ src/com/example/day05/util/Dice.java
@@ -3,8 +3,16 @@ package com.example.day05.util;
 import java.util.Random;
 
 public class Dice {
-    private int face = 6;
+    private int face;
     private int eye;
+
+    public Dice() {
+        this(6);
+    }
+
+    public Dice(int face) {
+        this.face = face;
+    }
 
     public void roll() {
         eye = new Random().nextInt(face) + 1;
